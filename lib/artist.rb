d01dbc736c8c89c4a43d50f46f222f0d5619a5aa
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-11 17:23:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :artist, :genre 
   
   def initialize(name)
     @name = name
@@ -12,7 +12,7 @@ class Artist
   
   def add_song(song)
     @songs << song 
-    song.artist
+    song.artist = self
   end
 end
   
