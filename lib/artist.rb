19dfb3f992072a93817b8262ca6d23f6d7bf8ff4
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-11 17:10:41 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+  end
+  
+  def add_song(song)
+    
+  end
+end
+  
